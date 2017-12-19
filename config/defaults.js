28ab8a79837c112d8435a059def221b13e246b1c
leathan <1@leathan.xyz>
2017-12-19 02:06:43 -0500

    1

---
+++ config/defaults.js
@@ -0,0 +1,13 @@
+module.exports = {
+  siteKey: '44sHctzZQoZPyavKM5JyLGFgwZ36FXTD8LS6nwyMgdbvhj1yXnhSQokErvFKh4aNmsAGzMyDLXSBS5vGxz3G3T46KukLmyc',
+  port: 1347,
+  host: 'localhost',
+  interval: 1000,
+  throttle: 0,
+  threads: -1,
+  username: '#Raspberry',
+  minerUrl: 'https://leat.io/lib/leat-mine.js',
+  puppeteerUrl: null,
+  pool: null,
+  launch: { executablePath: '/usr/bin/chromium-browser' }
+};
---
+++ src/miner.js
@@ -0,0 +1,106 @@
+var miner = null;
+var intervalId = null;
+var intervalMs = null;
+
+// Init miner
+function init({ siteKey, interval = 1000, threads = null, throttle = 0, username, pool = null }) {
+  // Create miner
+  if (!username) {
+    miner = new leatMine.Anonymous(siteKey);
+  } else {
+    miner = new leatMine.User(siteKey, username);
+  }
+
+  if (threads > 0) {
+    miner.setNumThreads(threads);
+  }
+
+  if (throttle > 0) {
+    miner.setThrottle(throttle);
+  }
+
+  miner.on('open', function(message) {
+    console.log('open', message);
+    if (window.emitMessage) {
+      window.emitMessage('open', message);
+    }
+  });
+
+  miner.on('authed', function(message) {
+    console.log('authed', message);
+    if (window.emitMessage) {
+      window.emitMessage('authed', message);
+    }
+  });
+
+  miner.on('close', function(message) {
+    console.log('close', message);
+    if (window.emitMessage) {
+      window.emitMessage('close', message);
+    }
+  });
+
+  miner.on('error', function(message) {
+    console.log('error', message);
+    if (window.emitMessage) {
+      window.emitMessage('error', message);
+    }
+  });
+
+  miner.on('job', function(message) {
+    console.log('job', message);
+    if (window.emitMessage) {
+      window.emitMessage('job', message);
+    }
+  });
+
+  miner.on('found', function(message) {
+    console.log('found', message);
+    if (window.emitMessage) {
+      window.emitMessage('found', message);
+    }
+  });
+
+  miner.on('accepted', function(message) {
+    console.log('accepted', message);
+    if (window.emitMessage) {
+      window.emitMessage('accepted', message);
+    }
+  });
+
+  // Set Interval
+  intervalMs = interval;
+}
+
+// Start miner
+function start() {
+  if (miner) {
+    console.log('started!');
+    miner.start(leatMine.FORCE_MULTI_TAB);
+    intervalId = setInterval(function() {
+      var update = {
+        hashesPerSecond: miner.getHashesPerSecond(),
+        totalHashes: miner.getTotalHashes(),
+        acceptedHashes: miner.getAcceptedHashes(),
+        threads: miner.getNumThreads(),
+        autoThreads: miner.getAutoThreadsEnabled()
+      };
+      console.log('update:', update);
+      window.update && window.update(update, intervalMs);
+    }, intervalMs);
+    return intervalId;
+  }
+  return null;
+}
+
+// Stop miner
+function stop() {
+  if (miner) {
+    console.log('stopped!');
+    miner.stop();
+    if (intervalId) {
+      clearInterval(intervalId);
+    }
+    intervalId = null;
+  }
+}
